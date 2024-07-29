068a28290d0f66d050ff241c58056c35692b8c26
Andrea Manini <a.mandini@vargroup.it>
2024-07-29 12:03:01 +0200

    feat: add demo app

---
+++ projects/front-office/src/app/app.component.ts
@@ -0,0 +1,36 @@
+import { Component, computed, HostBinding, inject, OnInit } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { TicketsDomainService } from '../../../../libs/tickets/domain/src/public-api';
+import { Luggage, LuggagesDomainService } from '../../../../libs/luggages/domain/src/public-api';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
+  })
+export class AppComponent implements OnInit{
+
+  title = 'front-office';
+
+  ticketSrv = inject(TicketsDomainService);
+  luggagesSrv = inject(LuggagesDomainService);
+
+  luggages = computed(() => {
+    return Object.entries(this.luggagesSrv.items()).map(([id, v]) => ({...v, id}))
+  })
+
+  ngOnInit(): void {
+      this.luggagesSrv.addLuggage(self.crypto.randomUUID(), {
+        dest: 'Paris',
+        ticketID: '1234',
+        weight: 10
+      })
+      this.luggagesSrv.addLuggage(self.crypto.randomUUID(), {
+        dest: 'London',
+        ticketID: '4557',
+        weight: 2
+      })
+  }
+}
