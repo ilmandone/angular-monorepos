8500fd7583096f55c6446de3164bee0a145ab329
Andrea Manini <a.mandini@vargroup.it>
2024-08-01 11:42:21 +0200

    feat(shell): use simple component as default route

--- projects/shell/src/app/app.routes.ts
+++ projects/shell/src/app/app.routes.ts
@@ -1,3 +1,19 @@
+import { loadRemoteModule } from '@angular-architects/native-federation';
+import { HttpClient } from '@angular/common/http';
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+	{
+		path: 'main',
+		loadComponent: () => loadRemoteModule('host', './SimpleComponent'),
+        providers: [
+            HttpClient
+        ]
+	},
+    {
+        path: '',
+        redirectTo: '/main',
+        pathMatch: 'full'
+    }
+    
+];
